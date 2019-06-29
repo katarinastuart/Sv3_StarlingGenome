65b847695b8314c556c71368c45fa797c86112a2
Katarina Stuart <katc.stuart@gmail.com>
2019-06-29 11:52:39 +1000

    Update Assembly_pipeline.bash

--- GenomeAssembly_Scripts/Assembly_pipeline.bash
+++ GenomeAssembly_Scripts/Assembly_pipeline.bash
@@ -47,6 +47,24 @@ SAMPLE=SV01
 
 supernova run --id svulgaris-10x --fastqs $FASTQ --sample $SAMPLE --description sturnus_vulgaris_10X --localcores 28 --localmem 900 --maxreads='all'
 
+#[error] The initially computed value for genome size is 1.27 Gb, from which we infer raw coverage of 85.5x. 
+#Our recommended range is between 38x and 56x. We test here for extremes of coverage outside the range 30x to 85x. 
+#If you have sufficient reads available in the original dataset, we recommend using --maxreads=474082919 (~56x) or a value at least --maxreads=321699123 (~38x).  
+#Therefore Supernova is aborting. Your options are: 
+#1) Reduce the --maxreads value that you provided on the supernova run  command line, and restart from the beginning. 
+#2) Alternatively, if you wish to continue on, without changes, you may do so by restarting supernova run with the option --accept-extreme-coverage, however this is not recommended and not supported. It may cause Supernova to crash, and in most cases will not yield a fully satisfactory assembly. 
+#Note that there are reasons why the computed genome size value may not agree with the genome size value that you believe is correct: 
+#1) The initially computed value is an approximation. The final value computed by Supernova is likely to be more accurate (although in rare cases it can be far off). 
+#2) In extreme cases, episome, microbiome DNA and contamination can significantly affect Supernova's genome size estimate.
+#
+#Saving pipestance info to svulgaris-10x/svulgaris-10x.mri.tgz
+#For assistance, upload this file to 10x Genomics by running:
+#
+#supernova upload <your_email> svulgaris-10x/svulgaris-10x.mri.tgz
+
+supernova run --id svulgaris-10x-56X --fastqs $FASTQ --sample $SAMPLE --description sturnus_vulgaris_10X-56X --localcores 28 --localmem 900 --maxreads=474082919
+
+
 
 #I think the fastqs need to be unzipped and sitting in a directory somewhere called fastq, which is what you give to the --fastqs setting. Eg.
 supernova run --id brownsnake-supernova2 --fastqs /srv/scratch/babsgenome/BABSGenome-Jun17/raw/2017-09-06.10xHiSeqX/fastq --sample 170814-FR07936203 --description brown_snake_10X-HiSeqX --localcores 28 --localmem 900
