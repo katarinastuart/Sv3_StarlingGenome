40f6eb60204bb36135804bf9bb6ce9cebaa084cd
Katarina Stuart <katc.stuart@gmail.com>
2019-08-16 14:56:33 +1000

    Add files via upload

--- GenomeAssembly_Scripts/Scaff10x_pipeline.bash
+++ GenomeAssembly_Scripts/Scaff10x_pipeline.bash
@@ -4,7 +4,7 @@
     
 #!/bin/bash
 
-#PBS -N 2019-08-15.Scaff10x
+#PBS -N 2019-08-16.Scaff10x
 #PBS -l nodes=1:ppn=32
 #PBS -l mem=180gb
 #PBS -l walltime=12:00:00
@@ -12,7 +12,7 @@
 #PBS -M katarina.stuart@student.unsw.edu.au
 #PBS -m ae
 
-cd /srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.2_Starling10x/scaff/
+cd /srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.2_Starling10x/scaff
 
 module add scaff10x
 
@@ -34,15 +34,16 @@ ${SCAFF10x}/scaff_BC-reads-2 sv10x-BC_1.name $READ2 sv10x-BC_2.fastq 2>&1 | tee 
 #Run Scaff10x
 #Tried a few things for the tiger snake but the basic Minimap mapping without running Break10x seemed to produce the same output as everything else: will just run that for now.
 
-ln /srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.3_GenomeAnnotation/data/GENOME.2018-08-21/Svulgaris_genomic.fna .
+#delete below line??
+#ln /srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.3_GenomeAnnotation/data/GENOME.2018-08-21/Svulgaris_genomic.fna .
 SREAD1=sv10x-BC_1.fastq
 SREAD2=sv10x-BC_2.fastq
 PPN=24
 GENOME=Svulgaris_genomic
-module add minimap2/2.14
-minimap2 -t $PPN -ax sr $PREFIX.fna $SREAD1 $SREAD2 > $PREFIX.10x.sam
+module add minimap2/2.16
+minimap2 -t $PPN -ax sr $GENOME.fna $SREAD1 $SREAD2 > $PREFIX.10x.sam
 
-CURDIR=/srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.2_Starling10x/scaff/
+CURDIR=/srv/scratch/z5188231/KStuart.Starling-Aug18/Sv3.2_Starling10x/scaff
 ${CURDIR}/scaffdir/scaff10x -nodes $PPN -matrix 2000 -reads 8 -score 20 -longread 1 -edge 50000 -link 6 -block 50000 -sam ${CURDIR}/${GENOME}.10x.sam $PREFIX.fna $SREAD1 $SREAD2 $GENOME.scaffolds.fasta 2>&1 | tee sv10x-Scaff10x.log
 
 SCAFF=${GENOME}.scaffolds
